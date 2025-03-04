1f10758cc966325e91094dd0014396d0eccde378
Pavel Sapronov <sapronovps@cloud-team.ru>
2025-03-04 17:06:27 +0300

    HW4 is completed

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,5 +1,7 @@
 package hw04lrucache
 
+import "sync"
+
 type Key string
 
 type Cache interface {
@@ -9,8 +11,7 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
+	mu       sync.Mutex
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
@@ -22,4 +23,48 @@ func NewCache(capacity int) Cache {
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
 	}
+}
+
+// Установка значения в кеш.
+func (cache *lruCache) Set(key Key, value interface{}) bool {
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+	if cache.capacity == 0 {
+		return false
+	}
+	existValue, ok := cache.items[key]
+	if ok {
+		cache.queue.Remove(existValue)
+		moveElem := cache.queue.PushFront(value)
+		cache.items[key] = moveElem
+	} else {
+		if cache.capacity > cache.queue.Len() {
+			lastElem := cache.queue.Back()
+			cache.queue.Remove(lastElem)
+		}
+		newElem := cache.queue.PushFront(value)
+		cache.items[key] = newElem
+	}
+
+	return ok
+}
+
+// Получение значения из кеша.
+func (cache *lruCache) Get(key Key) (interface{}, bool) {
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+	val, ok := cache.items[key]
+	if !ok {
+		return nil, false
+	}
+
+	cache.queue.MoveToFront(val)
+
+	return val.Value, true
+}
+
+// Очистка кеша.
+func (cache *lruCache) Clear() {
+	cache.items = make(map[Key]*ListItem, cache.capacity)
+	cache.queue = NewList()
 }
