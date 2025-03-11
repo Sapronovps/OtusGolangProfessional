0e80b06a2a28a21f3ebe67a57a0191b73eb35546
Pavel Sapronov <sapronovps@cloud-team.ru>
2025-03-11 17:00:14 +0300

    HW5 is completed

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,7 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +11,65 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	// n - количество горутин
+	// m - максимальное количество ошибок
+
+	tasksChan := make(chan Task)
+	errorsChan := make(chan struct{}, m)
+	quitChan := make(chan bool, n)
+
+	defer close(tasksChan)
+	defer close(errorsChan)
+	defer close(quitChan)
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go Worker(&wg, tasksChan, errorsChan, quitChan, m)
+	}
+
+	var err error
+	for index, task := range tasks {
+		if n+m == index {
+			if len(errorsChan) == m {
+				err = ErrErrorsLimitExceeded
+				break
+			}
+		}
+		tasksChan <- task
+	}
+
+	for i := 0; i < n; i++ {
+		quitChan <- true
+	}
+
+	wg.Wait()
+
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func Worker(wg *sync.WaitGroup, tasksChan <-chan Task, errorsChan chan<- struct{}, quitChan <-chan bool, m int) {
+	defer wg.Done()
+	for {
+		select {
+		case task, ok := <-tasksChan:
+			if !ok {
+				return
+			}
+			err := task()
+			if err != nil {
+				if len(errorsChan) == m {
+					return
+				}
+				errorsChan <- struct{}{}
+			}
+		case <-quitChan:
+			return
+		}
+	}
 }
