b39935c4d64f6221529dd2326afbc73060015faf
Pavel Sapronov <pavel.sapronov@vitech.team>
2025-03-19 08:29:30 +0300

    HW6 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,33 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	for _, stage := range stages {
+		stg := stage(in)
+		in = checkDone(stg, done)
+	}
+
+	return in
+}
+
+func checkDone(stg In, done In) Out {
+	out := make(Bi)
+
+	go func() {
+		defer func() {
+			close(out)
+		}()
+
+		for {
+			select {
+			case v, ok := <-stg:
+				if !ok {
+					return
+				}
+				out <- v
+			case <-done:
+				return
+			}
+		}
+	}()
+	return out
 }
