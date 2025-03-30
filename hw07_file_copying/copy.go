a2cd5f8b1740b45849192fa74ce7e69722f66732
Pavel Sapronov <pavel.sapronov@vitech.team>
2025-03-30 18:41:46 +0300

    HW7 in process

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
 )
 
 var (
@@ -10,6 +13,90 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	fromFile, fileSize, err := OpenReadFile(fromPath, offset)
+	if err != nil {
+		return err
+	}
+
+	toFile, err := os.Create(toPath)
+	if err != nil {
+		return fmt.Errorf("error create file: %w", err)
+	}
+
+	_, err = CopyFromTo(fromFile, toFile, limit, fileSize)
+
+	if err != nil {
+		return fmt.Errorf("error copy: %w", err)
+	}
+
+	if err := fromFile.Close(); err != nil {
+		return fmt.Errorf("error close file: %w", err)
+	}
+	if err := toFile.Close(); err != nil {
+		return fmt.Errorf("error close file: %w", err)
+	}
+
 	return nil
+}
+
+func OpenReadFile(path string, offset int64) (file *os.File, fileSize int64, err error) {
+	file, err = os.Open(path)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return file, 0, fmt.Errorf("error: %w", err)
+		}
+		return file, 0, err
+	}
+
+	stats, err := file.Stat()
+	if err != nil {
+		return file, 0, err
+	}
+
+	if stats.IsDir() {
+		return nil, 0, fmt.Errorf("%w: %s", ErrUnsupportedFile, path)
+	}
+
+	if offset > stats.Size() {
+		return file, 0, ErrOffsetExceedsFileSize
+	}
+
+	_, err = file.Seek(offset, io.SeekStart)
+	if err != nil {
+		return file, 0, err
+	}
+
+	return file, stats.Size(), nil
+}
+
+func CopyFromTo(fromFile io.Reader, toFile io.Writer, limit, fileSize int64) (n int64, err error) {
+	var needCopy = fileSize
+	var copied int64
+
+	if limit > 0 {
+		needCopy = limit
+	}
+
+	for {
+		n, err := io.CopyN(toFile, fromFile, needCopy)
+		copied += n
+
+		showProgress(copied, needCopy)
+		if needCopy <= copied {
+			break
+		}
+
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return copied, err
+		}
+	}
+
+	return copied, nil
+}
+
+func showProgress(copied, fileSize int64) {
+	fmt.Printf("\r%.2f%%", float64(copied)/float64(fileSize)*100)
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,41 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/require"
+	"os"
+	"testing"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	var inputFile = "testdata/input.txt"
+	var outputFile = "testdata/output.txt"
+
+	tests := []struct {
+		offset       int64
+		limit        int64
+		expectedFile string
+	}{
+		{offset: 0, limit: 0, expectedFile: "testdata/out_offset0_limit0.txt"},
+		{offset: 0, limit: 10, expectedFile: "testdata/out_offset0_limit10.txt"},
+		{offset: 0, limit: 1000, expectedFile: "testdata/out_offset0_limit1000.txt"},
+		{offset: 0, limit: 10000, expectedFile: "testdata/out_offset0_limit10000.txt"},
+		{offset: 100, limit: 1000, expectedFile: "testdata/out_offset100_limit1000.txt"},
+		{offset: 100, limit: 1000, expectedFile: "testdata/out_offset100_limit1000.txt"},
+		{offset: 6000, limit: 1000, expectedFile: "testdata/out_offset6000_limit1000.txt"},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.expectedFile, func(t *testing.T) {
+			err := Copy(inputFile, outputFile, tc.offset, tc.limit)
+			expectedFile, _ := os.ReadFile(tc.expectedFile)
+			resultFile, _ := os.ReadFile(outputFile)
+
+			if string(expectedFile) != string(resultFile) {
+				t.Errorf("expected: %s, got: %s", expectedFile, resultFile)
+			}
+			require.NoError(t, err)
+		})
+		_ = os.Remove(outputFile)
+	}
 }
