5a0b5310206af517236328384784c7c07e6781f0
Pavel Sapronov <pavel.sapronov@vitech.team>
2025-04-06 12:15:15 +0300

    HW8 is completed

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,43 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	tests := []struct {
+		nameEnv            string
+		expectedValue      interface{}
+		expectedNeedRemove bool
+	}{
+		{nameEnv: "EMPTY", expectedValue: "", expectedNeedRemove: false},
+		{nameEnv: "UNSET", expectedValue: "", expectedNeedRemove: true},
+		{nameEnv: "HELLO", expectedValue: "\"hello\"", expectedNeedRemove: false},
+		{nameEnv: "BAR", expectedValue: "bar", expectedNeedRemove: false},
+	}
+
+	mapEnv, err := ReadDir("testdata/env")
+	if err != nil {
+		fmt.Println(fmt.Errorf("read env dir error: %w", err))
+		return
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.nameEnv, func(t *testing.T) {
+			require.Equal(t, tc.expectedValue, mapEnv[tc.nameEnv].Value)
+			require.Equal(t, tc.expectedNeedRemove, mapEnv[tc.nameEnv].NeedRemove)
+		})
+	}
+}
+
+func TestInvalidDir(t *testing.T) {
+	t.Run("invalid dir", func(t *testing.T) {
+		_, err := ReadDir("testdata/env22")
+		require.Error(t, err)
+		require.ErrorContains(t, err, "error reading directory")
+	})
 }
--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,49 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	if len(cmd) == 0 {
+		fmt.Println(fmt.Errorf("error: empty command"))
+		return 1
+	}
+
+	if err := prepareEnvironment(env); err != nil {
+		fmt.Println(fmt.Errorf("environment preparation error: %w", err))
+		return 1
+	}
+
+	command := exec.Command(cmd[0], cmd[1:]...) //nolint:gosec
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+	command.Env = os.Environ()
+
+	if err := command.Run(); err != nil {
+		fmt.Println(fmt.Errorf("command execution error: %w", err))
+		return 1
+	}
+
+	return 0
+}
+
+// Подготовка переменных окружения.
+func prepareEnvironment(env Environment) error {
+	for name, params := range env {
+		if params.NeedRemove {
+			if err := os.Unsetenv(name); err != nil {
+				return fmt.Errorf("failed to unset %q: %w", name, err)
+			}
+			continue
+		}
+
+		if err := os.Setenv(name, params.Value); err != nil {
+			return fmt.Errorf("failed to set %q: %w", name, err)
+		}
+	}
+	return nil
 }
